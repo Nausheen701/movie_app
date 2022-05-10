79c273e8242e304e59e000f40e05a8fb089b4a86
Nausheen Akhter <nausheen701@gmail.com>
2022-05-10 13:27:43 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-
+import Person from './Person.js'
 
 
 const App = () => {
@@ -9,31 +9,36 @@ const App = () => {
 
   return (
     <div className="App">
+  
+     
+      <Person name={'Muhammad'} lastname={'B'} age={'9'}/>
+      <Person name={'Inaya'} lastname={'K'} age={'11'}/>
+      <Person name={'Hasan'} lastname={'K'} age={'9'}/>
+      <Person name={'Ali'} lastname={'D'} age={'8'}/>
+      <Person name={'Recep'} lastname={'H'} age={'9'}/>
       
-
-      <h1>Hello from {host}</h1>
-      {/* <h2>Hello {isNameShowing ? name : 'friend'}</h2> */}
-      <h3></h3>
-      {name ? (
-        <> 
-        {/* test */}
-        <h1>Hi {name}</h1>
-        </>
-      ) : ( 
-        <>
-        <h1>test</h1>
-        <h2>Who are you, friend?</h2> 
-        </>
-
-      )}
-<br></br>
-      <Person name={'Bilal'}/>
-      <Person />
-      <Person />
-      <Person />
-      <Person />
     </div>
-  );
+  )
+
+  // <h1>Salams from {host}</h1>
+  // {/* <h2>Hello {isNameShowing ? name : 'friend'}</h2> */}
+  // <h3></h3>
+  // {name ? (
+  //   <> 
+  //   {/* test */}
+  //   <h1>Hi {name}</h1>
+  //   </>
+  // ) : ( 
+  //   <>
+  //   <h1>test</h1>
+  //   <h2>Who are you, friend?</h2> 
+  //   </>
+
+  // )}
+
+
 }
 
 export default App
+
+
--- src/Person.js
+++ src/Person.js
@@ -5,9 +5,9 @@ const Person = (props) => {
     return (
       <>
       {/* <h1> First Name: Harry </h1> */}
-      <h1> First Name: Harry </h1>
-      <h1> Last Name: Potter </h1>
-      <h2> Age: 11 </h2>
+      <h1> First Name: {props.name} </h1>
+      <h1> Last Name: {props.lastname} </h1>
+      <h2> Age: {props.age} </h2>
   
       </>
     )
