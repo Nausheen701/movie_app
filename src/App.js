84483a505518503a23ecc26228b5586586f9b21d
Nausheen Akhter <nausheen701@gmail.com>
2022-05-10 12:27:42 +0300

    variables

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css'
 
+const Person
+
 const App = () => {
+  const name = 'Muhammad'
+  const host = 'Mama'
+  const isNameShowing = false
+
   return (
     <div className="App">
-      <h1>Hello from Nausheen</h1>
+      <h1>Hello from {host}</h1>
+      {/* <h2>Hello {isNameShowing ? name : 'friend'}</h2> */}
+      <h3></h3>
+      {name ? (
+        <> 
+        {/* test */}
+        <h1>Hi {name}</h1>
+        </>
+      ) : ( 
+        <>
+        <h1>test</h1>
+        <h2>Who are you, friend?</h2> 
+        </>
+        
+
+      
+      )}
     </div>
   );
 }
 
-export default App;
+export default App
